ce1449e0f882cd4d956bd1710ff0b61249eaeacb
tunghlls01-glitch <tunghlls01@gmail.com>
2026-07-15 03:12:37 +0700

    Update main.py

--- btvn01_ss16/main.py
+++ btvn01_ss16/main.py
@@ -1,3 +1,10 @@
+'''
+Tổng hợp 3 lỗi:
+Quan hệ	Lỗi	Cách sửa:
+1-1	Thiếu uselist=False và unique=True	Thêm uselist=False, unique=True
+1-N	back_populates="department_id" sai	Đổi thành back_populates="department"
+N-N	Thiếu secondary=student_course	Thêm secondary=student_course ở cả hai phía
+'''
 from fastapi import FastAPI
 import models
 from database import Base, engine
